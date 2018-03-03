e83916712128986a95200a50e1d933ae51d3865b
pjouzdani <p.jouzdani@knights.ucf.edu>
2018-03-03 11:24:27 -0500

    matrix of random numbers

--- DOS/BasicFunctions/generateNumberArray.py
+++ DOS/BasicFunctions/generateNumberArray.py
@@ -1,26 +1,29 @@
 # -*- coding: utf-8 -*-
 
-
 import numpy as np
 
-def generateNumberArray(N_spins, max_number):
-    num_baskets = (N_spins/max_number+1,
-                   N_spins/max_number)[N_spins -(N_spins/max_number)*max_number==0]
+def generateNumberArray(N_spins, max_n_spins_in_basket):
+    num_baskets = (N_spins/max_n_spins_in_basket+1,
+                   N_spins/max_n_spins_in_basket)\
+                   [N_spins-(N_spins/max_n_spins_in_basket)*\
+                    max_n_spins_in_basket==0]
     
-    num_spins_in_the_last_basket =  N_spins - (N_spins/max_number)*max_number
-    print num_baskets
-    print [max_number for i in range(N_spins/max_number)] + [num_spins_in_the_last_basket]
+#    print num_baskets
+#    print [max_n_spins_in_basket for i in range(N_spins/max_n_spins_in_basket)] + [num_spins_in_the_last_basket]
     numberArray=[]
-    for q in range(N_spins/max_number):
-        numberArray.append(np.random.randint(2**(max_number) ))
-    numberArray.append(np.random.randint(2**(num_spins_in_the_last_basket)) )
+    for q in range(N_spins/max_n_spins_in_basket):
+        numberArray.append(np.random.randint(2**(max_n_spins_in_basket) ))
+    if num_baskets!=(N_spins/max_n_spins_in_basket):
+        num_spins_in_the_last_basket =\
+        N_spins - (N_spins/max_n_spins_in_basket)*max_n_spins_in_basket
+        numberArray.append(np.random.randint(2**(num_spins_in_the_last_basket)))   
     return numberArray
     
     
     
 
-## testing
+# testing
 #N_spins = 10
-#max_number=6
-#numberArray = generateNumberArray(N_spins, max_number)
+#max_n_spins_in_basket=4
+#numberArray = generateNumberArray(N_spins, max_n_spins_in_basket)
 #print numberArray
--- DOS/FunctionsLayer1/getNumberMatrix.py
+++ DOS/FunctionsLayer1/getNumberMatrix.py
@@ -2,15 +2,52 @@
 
 import numpy as np
 
-#from DOS.
+from DOS.BasicFunctions.generateNumberArray import generateNumberArray
+
+
+def getSampleMatrix_unique(**args):
+    M = generateNumberMatrix(**args)
+    M_checker=M
+    
+    num_repeated_samples=0
+    for row_1 in range(M_checker.shape[0]-1):
+        for row_2 in range(row_1+1, M_checker.shape[0]):
+            col=0
+            
+            if M_checker[row_1, col]==M_checker[row_2, col]:
+                counter=1
+                for col in range(1, M_checker.shape[1]):
+                    if M_checker[row_1, col]==M_checker[row_2, col]:
+                        counter+=1
+                if counter==M_checker.shape[1]:
+                    num_repeated_samples+=1
+                    print row_1, row_2
+    return num_repeated_samples, M
+                
+
+
+
 #
 def generateNumberMatrix(**args):
     N_spins = args['N_spins']
-    max_number = args['max_number']
+    max_n_spins_in_basket = args['max_n_spins_in_basket']
     N_samples = args['N_samples']
-#    for s in range(N_samples):
+    samples_matrix=[]
+    for s in range(N_samples):
+        samples_matrix.append(generateNumberArray(N_spins, max_n_spins_in_basket))
+    samples_matrix = np.matrix(samples_matrix)
+    
+    return samples_matrix
         
     
-    
-    
+#### testing 
+
+args={}
+args['N_spins']=4
+args['max_n_spins_in_basket']=2
+args['N_samples']=10
+#M=generateNumberMatrix(**args)
+n, M=getSampleMatrix_unique(**args)
+print n
+print M
     
