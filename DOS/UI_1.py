bc580e04192b69a0e966516e65163109f4c25dd6
pjouzdani <p.jouzdani@knights.ucf.edu>
2018-03-04 22:17:25 -0500

    L2 Funcs - UI1 - test

--- DOS/UI_1.py
+++ DOS/UI_1.py
@@ -13,8 +13,8 @@ from DOS.FunctionsLayer2.getPartitionFunctionZ import getPartitionFunctionZ
 
 np.random.seed(1201)
 ### inputs
-N1=5
-N2=5
+N1=6
+N2=6
 
 args={}
 args['J_const']=1.0
@@ -33,18 +33,17 @@ args['first_neighb']=True
 neighbors_tables_list = constructLattice(**args)
 args['neighbors_table']=neighbors_tables_list
 
-args['max_n_spins_in_basket']=N1*N2
-args['N_samples']=2000
-args['num_bins']=2000
+args['max_n_spins_in_basket']=16  #N1*N2/10
+args['N_samples']=7000
+args['num_bins']=500
 ###########################
 sampleEnergy_array = getSampleEnergyArray(**args)
 ###
 energy_hist = getEnergyHist(sampleEnergy_array, **args)
 ###########################
 Temp_init = 10
-Temp_fin = 0.02
-num_temp=30
-num_avg=1
+Temp_fin = 0.001
+num_temp=40
 ###########################
 dTemp = (Temp_fin-Temp_init)* 1./num_temp
 Temps = [Temp_init + t*dTemp  for t in range(num_temp) ]
@@ -56,14 +55,12 @@ for b in range(num_temp):
     beta=1./Temp
     log_Z_beta=0
     energy_expct_beta=0
-    for s in range(num_avg):
-        ####
-        log_Z_beta+=np.log(getPartitionFunctionZ(energy_hist, beta, **args))
-        log_Z_beta=log_Z_beta*1./(N1*N2)*1./num_avg
-        ####
-        energy_expct_beta+=getEnergyExpectation(energy_hist, beta, **args)*\
-        1./(N1*N2)*1./num_avg
-        ####
+    log_Z_beta=np.log(getPartitionFunctionZ(energy_hist, beta, **args))
+    log_Z_beta=log_Z_beta*1./(N1*N2)
+    ####
+    energy_expct_beta=getEnergyExpectation(energy_hist, beta, **args)*\
+    1./(N1*N2)
+    ####
     ####
     log_Z_vs_temp[b, 0] = Temp
     log_Z_vs_temp[b, 1] = log_Z_beta
@@ -85,8 +82,9 @@ dEBeta_vs_temp[:, 1]= np.array([ energy_expectation_dt[b+1,1 ]*(Betas[b+1])\
 dEBeta_vs_temp[:, 0]=Temps[1:]
 
 dS = dEBeta_vs_temp[:,1] + dLogZ_vs_temp[:,1]
+S=[np.sum(dS[:t]) for t in range(len(dS)) ]
 fig, frame = plt.subplots(2,1, figsize=[10, 10])
-frame[0].plot(Temps[1:], dS[:]+np.log(2), '-o', label='entropy per spin - log(2)')
+frame[0].plot(Temps[1:], np.array(S)+np.log(2), '-o', label='entropy per spin - log(2)\nn_sample=%d'%args['N_samples'])
 frame[1].plot(Temps, energy_expectation_dt[:, 1], '-o', label='energy per spin')
 frame[0].legend()
 frame[1].legend()
---
+++ DOS/test.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+####
+
+### 7000, 5000, 3000
+x=[1./7000, 1./5000, 1./3000]
+entropy=[0.457,0.466,0.481]
+plt.plot(x, entropy, 'o')
+
+z = np.polyfit(x, entropy, 1)
+p = np.poly1d(z)
+plt.plot(x, entropy, 'o')
+plt.plot([0]+x, p([0]+x), '--')
